b21e52f5cefe82aa6731879588342d8fd7dc7c02
closerbibi <closerbibi@gmail.com>
2017-06-20 14:57:47 +0800

    make.sh update

--- make.sh
+++ make.sh
@@ -8,14 +8,17 @@ cd ../
 
 cd layers/reorg/src
 echo "Compiling reorg layer kernels by nvcc..."
-nvcc -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+#nvcc -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+nvcc -I/usr/local/cuda/include  -c -o reorg_cuda_kernel.cu.o reorg_cuda_kernel.cu \
+-x cu -Xcompiler -fPIC -arch=sm_52
 cd ../
 python build.py
 cd ../
 
 cd roi_pooling/src/cuda
 echo "Compiling roi_pooling kernels by nvcc..."
-nvcc -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu -x cu -Xcompiler -fPIC -arch=sm_52
+nvcc -I/usr/local/cuda/include -c -o roi_pooling_kernel.cu.o roi_pooling_kernel.cu \
+-x cu -Xcompiler -fPIC -arch=sm_52
 cd ../../
 python build.py
 cd ../
